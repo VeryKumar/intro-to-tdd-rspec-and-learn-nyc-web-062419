6b63a3e1675be9bb0f431ec86a5ff2f24487a776
Varun <uberkumar@gmail.com>
2019-06-04 01:56:26 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,5 @@
-age_of_person = current_age_of_birth_year(1984)
-def current_age_of_birth_year
-  2019 - 
+def current_age_of_birth_year(birth_year)
+  2019 - birth_year
 end
+
+age_of_person = current_age_of_birth_year(1984)
